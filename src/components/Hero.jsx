afda8c26211a8570486f074411aab593e2e4b748
Amrindra Ouk <oamrindra@gmail.com>
2023-05-26 19:46:38 +0000

    Structuing the Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,18 @@
+import { styles } from "../styles";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="relative w-full h-screen max-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[128px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
